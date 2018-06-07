4d896b2ef1c9614c45a0cb981b9cb05289afb16d
mampis99 <wfransm@gmail.com>
2018-06-07 21:31:24 +0700

    Rev

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -21,13 +21,34 @@ class LoginController extends Controller
 
       //dd($username." ".$password);
       $data = DB::table('r_login')
-                  ->where('status','=',1)
-                  ->where('username','=',$username)
-                  ->select('id_user','username','role','status')
+                  ->where('r_login.status','=',1)
+                  ->where('r_login.username','=',$username)
+                  ->join('m_siswa','r_login.id_user','=','m_siswa.id_siswa')
+                  ->select('r_login.id_user','m_siswa.nm_siswa','r_login.username','r_login.password','r_login.role','r_login.status')
                   ->first();
+      dd($data);
       if (count($data)>0)
       {
-        return "ada";
+        if (Hash::check($password, Hash::make($data->password)))
+        {
+          if ($data->role == "siswa")
+          {
+            Session::put('id',$data->id_siswa);
+            Session::put('username',$data->username);
+            Session::put('password',$password);
+            return redirect('/dashboard/siswa');
+          }
+          if ($data->role == "guru")
+          {
+            //Session::put('id',$data->id_siswa);
+            //Session::put('username',$data->username);
+            //Session::put('password',$password);
+            return "halaman guru";
+          }
+        }
+        else {
+          return "pass salah";
+        }
       }
       else
       {
